e25b4cd748ba5f3d284232ace116e7e45dd59c3c
vtwqajjq <vitywqajjq@icloud.com>
2022-02-16 14:15:12 +0300

    First day of Challenge

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+}
+
+function playSound(e) {
+    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing')
+}
+
+window.addEventListener('keydown', playSound)
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
